41e9271d0180acbbb319cd92c98e8f4285cfd81a
Nishchal Gautam <gautam.nishchal@gmail.com>
2016-02-19 10:33:47 +0700

    refactored

--- html/js/app_react.js
+++ html/js/app_react.js
@@ -22,32 +22,30 @@ var backgroundLightF5F5={
 }
 var Login = React.createClass({
 			post_login:function(response){
-				console.log(response.auth_token);
 				Cookies.set('auth-token',response.auth_token);
 			},
 			componentDidMount: function(){
 				// window.history.pushState("login", "Login", "/login");
 				startApp(this.post_login);
-				document.getElementById('svg');
-					var tl = new TimelineMax({
-						repeat: -1
-					});
+				var tl = new TimelineMax({
+					repeat: -1
+				});
 
-					tl.set('#outline', {
-						drawSVG: '0% 0%'
-					})
-					.to('#outline', 0.2, {
-						drawSVG: '11% 25%',
-						ease: Linear.easeNone
-					})
-					.to('#outline', 0.5, {
-						drawSVG: '35% 70%',
-						ease: Linear.easeNone
-					})
-					.to('#outline', 0.9, {
-						drawSVG: '99% 100%',
-						ease: Linear.easeNone
-					})
+				tl.set('#outline', {
+					drawSVG: '0% 0%'
+				})
+				.to('#outline', 0.2, {
+					drawSVG: '11% 25%',
+					ease: Linear.easeNone
+				})
+				.to('#outline', 0.5, {
+					drawSVG: '35% 70%',
+					ease: Linear.easeNone
+				})
+				.to('#outline', 0.9, {
+					drawSVG: '99% 100%',
+					ease: Linear.easeNone
+				})
 			},
 			googleClicked:function(){
 				$('#loader').show();
